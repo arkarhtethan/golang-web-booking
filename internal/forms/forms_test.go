2871220d1945712cc77abf9342d39c4fab977d9a
Kaung Myat Han <arkarhtethan@gmail.com>
2022-08-01 23:15:47 +0630

    form testing added

---
+++ internal/forms/forms_test.go
@@ -0,0 +1,127 @@
+package forms
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"net/url"
+	"testing"
+)
+
+func TestForm_Valid(t *testing.T) {
+	r := httptest.NewRequest("POST", "/whatever", nil)
+	form := New(r.PostForm)
+
+	isValid := form.Valid()
+	if !isValid {
+		t.Error("got invalid when should have been valid")
+	}
+}
+
+func TestForm_Required(t *testing.T) {
+	r := httptest.NewRequest("POST", "/whatever", nil)
+	form := New(r.PostForm)
+
+	form.Required("a", "b", "c")
+	if form.Valid() {
+		t.Error("form shows valid when required fields missing")
+	}
+
+	postedData := url.Values{}
+	postedData.Add("a", "a")
+	postedData.Add("b", "a")
+	postedData.Add("c", "a")
+
+	r, _ = http.NewRequest("POST", "/whatever", nil)
+
+	r.PostForm = postedData
+	form = New(r.PostForm)
+	form.Required("a", "b", "c")
+	if !form.Valid() {
+		t.Error("shows does not have required fields when it does")
+	}
+}
+
+func TestForm_Has(t *testing.T) {
+	postedData := url.Values{}
+	form := New(postedData)
+
+	has := form.Has("whatever")
+	if has {
+		t.Error("form show has field when it does not")
+	}
+
+	postedData = url.Values{}
+	postedData.Add("a", "a")
+	form = New(postedData)
+
+	has = form.Has("a")
+	if !has {
+		t.Error("shows form does not have field when it should")
+	}
+}
+
+func TestForm_MinLength(t *testing.T) {
+	postedData := url.Values{}
+	form := New(postedData)
+
+	form.MinLength("whatever", 10)
+	if form.Valid() {
+		t.Error("form show min length for non existance field")
+	}
+
+	isError := form.Errors.Get("whatever")
+
+	if isError == "" {
+		t.Error("should have an error, but didn't get one")
+	}
+
+	postedData = url.Values{}
+	postedData.Add("some_field", "some value")
+	form = New(postedData)
+
+	form.MinLength("some_field", 100)
+	if form.Valid() {
+		t.Error("form show min length of 100 when data is shorter")
+	}
+	postedData = url.Values{}
+	postedData.Add("another_field", "abc123")
+	form = New(postedData)
+
+	form.MinLength("another_field", 1)
+	if !form.Valid() {
+		t.Error("show minlength of 1 is not met when it is.")
+	}
+
+	isError = form.Errors.Get("another_field")
+
+	if isError != "" {
+		t.Error("should not have an error, but get one")
+	}
+}
+
+func TestForm_IsEmail(t *testing.T) {
+	postedData := url.Values{}
+	form := New(postedData)
+
+	form.IsEmail("x")
+	if form.Valid() {
+		t.Error("form show email for non existance field")
+	}
+
+	postedData = url.Values{}
+	postedData.Add("email", "me@here.com")
+	form = New(postedData)
+
+	form.IsEmail("email")
+	if !form.Valid() {
+		t.Error("got an invalid email when we should not have.")
+	}
+	postedData = url.Values{}
+	postedData.Add("another_field", "asdfadfkj")
+	form = New(postedData)
+
+	form.IsEmail("another_field")
+	if form.Valid() {
+		t.Error("got valid for nivalid email.")
+	}
+}
