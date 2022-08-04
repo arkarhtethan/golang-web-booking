6fed858c876ad8504cd44df8b35157d4f75e2f17
Kaung Myat Han <arkarhtethan@gmail.com>
2022-08-04 21:37:07 +0630

    models added

--- internal/models/models.go
+++ internal/models/models.go
@@ -1,8 +1,55 @@
 package models
 
+import "time"
+
 type Reservation struct {
 	FirstName string
 	LastName  string
 	Email     string
 	Phone     string
+	StartDate time.Time
+	EndDate   time.Time
+	CreatedAt time.Time
+	UpdatedAt time.Time
+	RoomID    int
+	Room      Room
+}
+
+type User struct {
+	ID          int
+	FirstName   string
+	LastName    string
+	Email       string
+	Password    string
+	AccessLevel int
+	CreatedAt   time.Time
+	UpdatedAt   time.Time
+}
+
+type Room struct {
+	ID        int
+	RoomName  string
+	CreatedAt time.Time
+	UpdatedAt time.Time
+}
+
+type Restriction struct {
+	ID              int
+	RestrictionName string
+	CreatedAt       time.Time
+	UpdatedAt       time.Time
+}
+
+type RoomRestriction struct {
+	ID            int
+	RoomID        int
+	ReservationID int
+	RestrictionID int
+	StartDate     time.Time
+	EndDate       time.Time
+	CreatedAt     time.Time
+	UpdatedAt     time.Time
+	Room          Room
+	Reservation   Reservation
+	Restriction   Restriction
 }
